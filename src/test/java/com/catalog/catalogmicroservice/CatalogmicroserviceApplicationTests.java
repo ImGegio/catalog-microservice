a5a4e77c60cc9885b90a0c1d151ffd1b87975ca8
LorenzoElia95 <lorenzo.elia.pro@gmail.com>
2024-04-02 18:18:44 +0200

    Initial config

---
+++ src/test/java/com/catalog/catalogmicroservice/CatalogmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog.catalogmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
